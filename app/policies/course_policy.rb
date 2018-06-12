e15fc5818c7be4d107ad026c179c72f61d8a960a
Sven Werner <vegascowboy@web.de>
2018-06-12 19:14:56 +0200

    Added Gem Pundit; Added Authentication for Courses

---
+++ app/policies/course_policy.rb
@@ -0,0 +1,34 @@
+class CoursePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    !user.nil?
+  end
+
+  def new?
+    !user.nil?
+  end
+
+  def edit?
+    is_owner?
+  end
+
+  def update?
+    is_owner?
+  end
+
+  def destroy?
+    is_owner?
+  end
+
+  private
+
+  def is_owner?
+    @record.user == @user
+  end
+
+end
