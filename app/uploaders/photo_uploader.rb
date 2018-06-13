0390b251246f5bef2040d35dccefb06260b534bf
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-06-13 10:53:49 +0200

    Photo uploader for courses done

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,5 +1,11 @@
 class PhotoUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
 
+    "http://res.cloudinary.com/djjs4pnpf/image/upload/v1528878490/jpsez3ep8okeusjdqinz.jpg"
+  end
 end
