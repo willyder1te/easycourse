00850b034da57b22ec7264dd0939a6bb7e7669c1
Willy Koeplin <willykoeplin@gmail.com>
2018-06-14 18:47:23 +0200

    added search filters fr location, max price and start and end times

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -40,6 +40,29 @@ class CoursesController < ApplicationController
   def index
     @courses = policy_scope(Course)
     @courses = Course.where.not(latitude: nil, longitude: nil)
+    if params["location"] != "" &&  params["location"] != nil
+      @courses = Course.near(params["location"], 25)
+    end
+
+    if params["category"] != "" && params["category"] != nil
+      category = params["category"]
+      @courses = @courses.reject { |course| course.category.downcase != category }
+    end
+
+    if params["max-price"] != "" && params["max-price"] != nil
+      max_price = params["max-price"].to_f
+      @courses = @courses.reject { |course| course.price > max_price }
+    end
+
+    if params["start_time"] != "" && params["start_time"] != nil
+      start_time = params["start_time"]
+      @courses = @courses.reject { |course| course.start_date < start_time }
+    end
+
+    if params["end_time"] != "" && params["end_time"] != nil
+      end_time = params["end_time"]
+      @courses = @courses.reject { |course| course.end_date > end_time }
+    end
 
     @markers = @courses.map do |course|
       {
