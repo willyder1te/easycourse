5a51e77edf6724e908b3eddfcc4258cc133d6216
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-06-15 15:25:07 +0200

    Revert "Fixed some styles"
    This reverts commit aa6456807c7a58ac8be1e89ed61092cbb1b8bc88.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
